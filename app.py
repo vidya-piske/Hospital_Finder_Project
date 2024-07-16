7f4463f681f73473d697def6e0c81a81544972dd
vidya-piske <vidyapiske123@gmail.com>
2024-07-16 09:57:32 +0530

    Added Cors in the app.py file

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from services.geo_service import geo_service_app
 from services.places_service import places_service_app
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.register_blueprint(geo_service_app, url_prefix='/api')
 app.register_blueprint(places_service_app, url_prefix='/api')
 
